f79906eadcbeecaf0ced103fcc88b0b8282216f9
etharani <141474110+etharani@users.noreply.github.com>
2023-08-08 22:54:04 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,16 @@
+class Area{
+ double a=3.0;
+ double b=15.5;
+ void rectangle (){
+	 double c=a*b;
+	  if(c>30){
+	  System.out.println("True");}
+	  else{
+ System.out.println("False");}}
+public static void main (String args[]){
+	Area a1=new Area();
+	a1.rectangle();
+	
+	
+}}
+		  
